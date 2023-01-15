daf2098336a5ab869de849ede8a1e568312cf9f4
AnwarBrini <anwar_brini@outlook.fr>
2023-01-15 23:59:45 +0100

    sql added date filter

--- test_2.sql
+++ test_2.sql
@@ -23,6 +23,7 @@ sum(prod_price * prod_qty)
 From (
 Select trs.date, trs.client_id, trs.prop_id, trs.prod_price, trs.prod_qty, pn.product_type
 From TRANSACTION trs
+Where  trs.Date >= '01/01/2019' and trs.Date <= '31/12/2019'
 Left join 	PRODUCT_NOMENCLATURE pn
 On trs.prop_id = pn.product_id ) enriched_transactions
 
